a69c8a94e129aea3164c97e39cab8d172d2281dd
Nakmu-git <113244551+nakamu-1@users.noreply.github.com>
2022-09-20 06:56:08 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+
+'use strict';
+
+{
+  const x = 100;
+  console.log(x);
+}
+// function sum(a, b, c) {
+  // console.log(a + b + c);
+//   const sum = function(a, b, c) {
+//   return a + b + c;
+// };
+
+// const sum = (a, b, c) => a + b + c;
+
+// // sum(1, 2, 3);
+// // sum(3, 4, 5);
+
+// const total = sum(1, 2, 3) + sum(3, 4, 5);
+// console.log(total);
+
+// const double = function(a) {
+//   return a * 2
+// };
+
+const double = (a) => a * 2;
+console.log(double(12));
